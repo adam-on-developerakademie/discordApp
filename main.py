8bdfaf4e7bd001c4470f9f1277d477c7a8f75fb1
adamp <adam.piskorek@gmx.de>
2025-12-14 05:04:26 +0100

    Logging handler added

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import discord
 from dotenv import load_dotenv
 import os
 import discord.ext.commands as commands
+import logging
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 # print(f"Mein Token ist: {token}")
+
+# Logging handler:
+handler = logging.FileHandler(filename='discordBot.log', encoding='utf-8', mode='w')
+
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -15,4 +20,14 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Bot ist eingeloggt als {bot.user}') 
     
-bot.run(token)
+    
+
+
+    
+    
+    
+    
+    
+    
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
