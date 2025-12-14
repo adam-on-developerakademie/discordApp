aa045eb3724675ded5b9e023ae001be0918afe2e
adamp <adam.piskorek@gmx.de>
2025-12-14 04:53:01 +0100

    bot.run(token)

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import discord
 from dotenv import load_dotenv
 import os
+import discord.ext.commands as commands
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-print(f"Mein Token ist: {token}")
+# print(f"Mein Token ist: {token}")
 
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Bot ist eingeloggt als {bot.user}') 
+    
+bot.run(token)
